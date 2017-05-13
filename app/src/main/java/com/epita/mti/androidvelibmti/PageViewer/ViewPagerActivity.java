03e855223492ca4e8b079a97bb0c63bb272b52e8
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-05-13 21:50:48 +0200

    Add better detail view

--- app/src/main/java/com/epita/mti/androidvelibmti/PageViewer/ViewPagerActivity.java
+++ app/src/main/java/com/epita/mti/androidvelibmti/PageViewer/ViewPagerActivity.java
@@ -1,26 +1,33 @@
 package com.epita.mti.androidvelibmti.PageViewer;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.epita.mti.androidvelibmti.Authors.AuthorsActivity;
+import com.epita.mti.androidvelibmti.MainActivity;
 import com.epita.mti.androidvelibmti.R;
 
 /**
  * Created by hadri on 13/05/2017.
  */
 
-public class ViewPagerActivity extends FragmentActivity {
+public class ViewPagerActivity extends AppCompatActivity {
 
 
     private static final int NUM_PAGES = 5;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,33 @@ public class ViewPagerActivity extends FragmentActivity {
         mPager.setPageTransformer(true, new ZoomOutPageTransformer());
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+        initToolBar();
+    }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.authors:
+                showAuthors();
+                break;
+            case R.id.share:
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -43,6 +77,12 @@ public class ViewPagerActivity extends FragmentActivity {
             // Otherwise, select the previous step.
             mPager.setCurrentItem(mPager.getCurrentItem() - 1);
         }
+    }
+
+    public void showAuthors()
+    {
+        Intent intent = new Intent(ViewPagerActivity.this, AuthorsActivity.class);
+        startActivity(intent);
     }
 
     /**
