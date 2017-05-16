45b0b8f15df2fdf221ef7627fc97fc1932800f68
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-05-16 15:56:07 +0200

    Fix bug on detail view

--- app/src/main/java/com/epita/mti/androidvelibmti/PageViewer/PlaceholderFragment.java
+++ app/src/main/java/com/epita/mti/androidvelibmti/PageViewer/PlaceholderFragment.java
@@ -1,7 +1,9 @@
 package com.epita.mti.androidvelibmti.PageViewer;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +12,10 @@ import android.widget.TextView;
 import com.epita.mti.androidvelibmti.DBO.Station;
 import com.epita.mti.androidvelibmti.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.TimeZone;
 
 /**
  * Created by hadri on 13/05/2017.
@@ -20,7 +25,12 @@ public class PlaceholderFragment extends Fragment {
     public static final String ARG_PAGE = "STATION_ID";
     private Station mStation;
 
-    TextView mTextView;
+    TextView mTextViewName;
+    TextView mTextViewLocation;
+    TextView mTextViewUpdateDate;
+    TextView mTextViewBikeTotal;
+    TextView mTextViewBikeAvailable;
+    TextView mTextViewStatus;
 
     public static PlaceholderFragment create(Station station) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -45,10 +55,60 @@ public class PlaceholderFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_main, container, false);
 
+        mTextViewName = (TextView)rootView.findViewById(R.id.section_label);
+        mTextViewLocation = (TextView)rootView.findViewById(R.id.StationLocationText);
+        mTextViewUpdateDate = (TextView)rootView.findViewById(R.id.LastUpdateText);
+        mTextViewBikeTotal = (TextView)rootView.findViewById(R.id.bikeStatsTextTotal);
+        mTextViewBikeAvailable = (TextView)rootView.findViewById(R.id.bikeStatsTextAvailable);
+        mTextViewStatus = (TextView)rootView.findViewById(R.id.Status);
 
-        mTextView = (TextView)rootView.findViewById(R.id.section_label);
+        mTextViewName.setText(String.valueOf(mStation.getName()));
 
-        mTextView.setText(String.valueOf(mStation.getName()));
+        if (mStation.getStatus().equals("CLOSED")) {
+            mTextViewStatus.setText("FERMER");
+            mTextViewStatus.setTextColor(Color.RED);
+        }
+        else {
+            mTextViewStatus.setText("OUVERT");
+            mTextViewStatus.setTextColor(Color.GREEN);
+        }
+
+        mTextViewLocation.setText(String.valueOf(mStation.getAddress()));
+
+
+        try {
+            String dateFormat = String.valueOf(mStation.getLast_update());
+            int ind = dateFormat.lastIndexOf(":");
+            if(ind >= 0) {
+                dateFormat = new StringBuilder(dateFormat).replace(ind, ind + 1, "").toString();
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            long time = 0;
+            time = sdf.parse(String.valueOf(dateFormat)).getTime();
+            long now = System.currentTimeMillis();
+            CharSequence ago =
+                    DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS);
+            mTextViewUpdateDate.setText(ago);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            mTextViewUpdateDate.setText(String.valueOf(mStation.getLast_update()));
+        }
+
+
+
+
+        mTextViewBikeAvailable.setText(String.valueOf(mStation.getAvailable_bike_stands()));
+        if (mStation.getAvailable_bike_stands() <= 0) {
+            mTextViewBikeAvailable.setTextColor(Color.RED);
+        }
+        else {
+            mTextViewBikeAvailable.setTextColor(Color.GREEN);
+        }
+        String TotalBikeText = " / " + String.valueOf(mStation.getBike_stands());
+        mTextViewBikeTotal.setText(TotalBikeText);
+
+
         return rootView;
     }
 
